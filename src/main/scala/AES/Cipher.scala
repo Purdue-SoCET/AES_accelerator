6a8942b304eef8f1b4a3e44aa9c4d9c46dcbe5b5
Seongjoong Yim <yim13@asicfab.ecn.purdue.edu>
2025-07-11 15:35:27 -0400

    cipher waiting for submodules

--- src/main/scala/AES/Cipher.scala
+++ src/main/scala/AES/Cipher.scala
@@ -10,9 +10,10 @@ class Cipher(keySize: Int) extends Module {
     val io = IO(new Bundle {
         val nRST            = Input(Bool())
         val in              = Input(Bits(128.W))
+        val load            = Input(Bool())
         val key             = Input(Bits(keySize.W))
         val out             = Output(Bits(128.W))
-        val outputValid     = Output(Bool())
+        val done            = Output(Bool())
     })
 
     def getNr(keySize: Int): Int = keySize match {
@@ -23,12 +24,54 @@ class Cipher(keySize: Int) extends Module {
     }
     val Nr = getNr(keySize)
 
+    // Initialize modules
     val keyExpansion = Module(new keyExpansion())
-    val initialADD = Module(new AddRoundKey())
+    val initialAddRoundKey = Module(new AddRoundKey())
+    val roundUnitVec = VecInit(Seq.fill(Nr - 1)(new Module(RoundUnit())))
+    val lastSubByte = Module(new SubByte())
+    val lastShiftRows = Module(new ShiftRows())
+    val lastAddRoundKey = Module(new AddRoundKey())
 
-    val pipeline = withReset(!io.Input.nRST) {
-        Seq.fill(1 + Nr)(Seq.fill(4)(Seq.fill(4)(RegInit(0.U(8.W)))))
+    // Initialize pipeline reigsters
+    val pipeline = withReset(~io.Input.nRST) {
+        RegInit(VecInit(Seq.fill(Nr + 1)(VecInit(Seq.fill(4)(VecInit(Seq.fill(4)(0.U(8.W))))))))
+    }
+    val valid = withReset(~io.Input.nRST) {
+        RegInit(VecInit(Seq.fill(Nr + 1)(0.U(1.W))))
     }
 
-    val roundUnits = Seq.fill(Nr - 1)(new Module(RoundUnit()))
+    // Convert in to 4x4 vec to feed to initialAddRoundKey module
+    val inVec = Wire(Vec(4, Vec(4, Bits(8.W))))
+    for(row <- 0 until 4) {
+        for(col <- 0 until 4) {
+            inVec(row)(col) := io.in(127 - (row + col * 4) * 8, 120 - (row + col * 4) * 8)
+        }
+    }
+    
+    // Connect wires
+    // TODO - Input roundKey to initialAddRoundKey module
+    initialAddRoundKey.io.in := inVec
+
+    pipeline(0) := initialAddRoundKey.io.out
+    valid(0)    := io.load
+
+    for(i <- 0 until Nr - 1) {
+        roundUnitVec(i).io.in   := pipeline(i)
+        pipeline(i + 1)         := roundUnitVec(i).io.out
+        // TODO - Input roundKey to RoundUnit
+    }
+
+    for(i <- 1 to Nr) {
+        valid(i) := valid(i - 1)
+    }
+
+    lastSubByte.io.in       := pipeline(Nr - 1)
+    lastShiftRows.io.in     := lastSubByte.io.out
+    lastAddRoundKey.io.in   := lastShiftRows.io.out
+    // TODO - Input roundKey to lastAddRoundKey module
+
+    pipeline(Nr) := lastAddRoundKey.io.out
+
+    io.out  := pipeline(Nr)
+    io.done := valid(Nr)
 }
