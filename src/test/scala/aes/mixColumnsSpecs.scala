415f1f18666b05394b02747125531b20eb454fc6
Jai Anand Keskar <jkeskar@asicfab.ecn.purdue.edu>
2025-07-24 13:16:19 -0400

    Mix Columns big int issue with input

---
+++ src/test/scala/aes/mixColumnsSpecs.scala
@@ -0,0 +1,54 @@
+package aes
+
+import chisel3._
+import chisel3.simulator.scalatest.ChiselSim
+import org.scalatest.freespec.AnyFreeSpec
+
+class shiftRowsSpec extends AnyFreeSpec with ChiselSim {
+    
+    "Mix Rows mixed" in {
+        simulate(new mixColumns(4)) { dut =>
+            // Key and text needs to be 128 bits, 16 words
+            val input = Seq(Seq(1,2,3,4), Seq(5,6,7,8), Seq(9,10,11,12), Seq(13,14,15,16))
+            val expected = Seq(Seq(39,46,53,60), Seq(51,58,65,72), Seq(63,70,77,84), Seq(43,50,57,64))
+
+
+            // Feed input
+            for (col <- 0 until 4; row <- 0 until 4) {
+                dut.io.in(col)(row).poke(input(col)(row).U)
+            }
+
+            for (col <- 0 until 4) {
+                for(row <- 0 until 4) {
+                    print(input(col)(row))
+                    print(" ")
+                }
+                print("\n")
+            }
+
+            print("\n")
+            for (col <- 0 until 4) {
+                for(row <- 0 until 4) {
+                    print(expected(col)(row))
+                    print(" ")
+                }
+                print("\n")
+            }
+
+            print("\n")
+            for (col <- 0 until 4) {
+                for(row <- 0 until 4) {
+                    print(dut.io.out(col)(row).peek().litValue)
+                    print(" ")
+                }
+                print("\n")
+            }
+
+            // Expected output
+            for (col <- 0 until 4; row <- 0 until 4) {
+                dut.io.out(col)(row).expect(expected(col)(row).U)
+                // print("Col : " + col + " Row : " + row)
+            }
+        }
+    }
+}
