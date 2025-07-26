a8958352ba69bcd600d22c76da958ee816c89ab3
Kameron Jackson <142923032+jacks908@users.noreply.github.com>
2025-07-26 18:22:44 -0400

    Create KeyExpansion.scala

---
+++ src/main/scala/AES/KeyExpansion.scala
@@ -0,0 +1,101 @@
+package aes
+
+import chisel3._
+import chisel3.util._
+
+/** KeyExpansion module for AES that handles 128/192/256-bit keys */
+class KeyExpansion(keySize: Int) extends Module {
+  require(Seq(128, 192, 256).contains(keySize), "Invalid key size")
+  val io = IO(new Bundle {
+    val keyIn = Flipped(Decoupled(UInt(keySize.W)))   // Input key: 128, 192, or 256 bits
+    val roundKeyOut = Decoupled(UInt(128.W))          // Stream of round keys (128-bit chunks)
+    val done        = Output(Bool())                  // high when last round key is done
+  })
+
+  // === Default I/O assignments ===
+  io.keyIn.ready := false.B
+  io.roundKeyOut.valid := false.B
+  io.roundKeyOut.bits := 0.U
+  io.done := false.B
+
+  // === Module instantiations ===
+  val gFunc = Module(new GFunction)
+  val sbox0 = Module(new SBox)
+  val sbox1 = Module(new SBox)
+  val sbox2 = Module(new SBox)
+  val sbox3 = Module(new SBox)
+
+  gFunc.io.in := 0.U
+  gFunc.io.round := 0.U
+  sbox0.io.in := 0.U
+  sbox1.io.in := 0.U
+  sbox2.io.in := 0.U
+  sbox3.io.in := 0.U
+
+  // === Helper: SubWord using SBoxes ===
+  def subWordOnly(word: UInt): UInt = {
+    sbox0.io.in := word(31, 24)
+    sbox1.io.in := word(23, 16)
+    sbox2.io.in := word(15, 8)
+    sbox3.io.in := word(7, 0)
+    Cat(sbox0.io.out, sbox1.io.out, sbox2.io.out, sbox3.io.out)
+  }
+
+  // === Setup ===
+  val wordCount = keySize / 32  // number of 32-bit words in the input key
+  val Nr = keySize match {      // Match the keysize to the round
+    case 128 => 10
+    case 192 => 12
+    case 256 => 14
+  }
+  val totalRoundKeys = 4 * (Nr + 1)                 // Total amount of round keys/words
+
+  val words = Reg(Vec(totalRoundKeys, UInt(32.W)))  // splits intial key into vector (32 bit per element) 
+  val loaded = RegInit(false.B)                     // Flag: have we accepted the input key?   
+
+  when(io.keyIn.fire && !loaded) {
+    for (j <- 0 until wordCount) {
+      words(j) := io.keyIn.bits((keySize - 1 - j * 32), keySize - (j + 1) * 32)
+    }
+    loaded := true.B
+  }
+
+  val i = RegInit(wordCount.U(log2Ceil(totalRoundKeys).W))    // starts after initial key
+  val roundIndex = RegInit(0.U(log2Ceil(Nr + 2).W))           // to count 0 to Nr
+
+  // === Key expansion logic ===
+  when(loaded && i < totalRoundKeys.U) {
+    val prev = words(i - 1.U)                                 // w3-128,w5-192,w7-256
+    val prevNk = words(i - wordCount.U)                       // w0-128,w0-192,w0-256
+    val temp = Wire(UInt(32.W))
+
+    when(i % wordCount.U === 0.U) {
+      gFunc.io.in := prev
+      gFunc.io.round := (i / wordCount.U)(3, 0) //Gfunction Case
+      temp := gFunc.io.out
+    }.elsewhen(wordCount.U > 6.U && i % wordCount.U === 4.U) {
+      temp := subWordOnly(prev) //Sbox case
+    }.otherwise {
+      temp := prev
+    }
+
+    words(i) := prevNk ^ temp // ACTUAL OPERATION
+    i := i + 1.U
+  }
+
+  // === Output round key ===
+  when(loaded && i >= ((roundIndex + 1.U) * 4.U)) {
+    io.roundKeyOut.valid := true.B
+    io.roundKeyOut.bits := Cat(
+      words(roundIndex * 4.U + 0.U),
+      words(roundIndex * 4.U + 1.U),
+      words(roundIndex * 4.U + 2.U),
+      words(roundIndex * 4.U + 3.U)
+    )
+    when(io.roundKeyOut.ready) {
+      roundIndex := roundIndex + 1.U
+    }
+  }
+
+  io.done := roundIndex === (Nr + 1).U
+}
